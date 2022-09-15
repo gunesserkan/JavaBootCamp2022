9c8a25b67f317000b9ee4d99958df7ac74bfe9e2
Serkan Güneş <gunesserkan883@gmail.com>
2022-09-15 20:39:54 +0300

    Ödev2 video18 String örnekleri 2

---
+++ BootCampGun1Odevler/src/com/odev2/video18/Main.java
@@ -0,0 +1,28 @@
+package com.odev2.video18;
+
+public class Main {
+
+	//STRINGLERLE CALİSMA 2
+	public static void main(String[] args) {
+		
+		String mesaj = "Bugün hava çok güzel.";
+		System.out.println(mesaj);
+		
+		String yeniMesaj = mesaj.replace(' ','-');
+		System.out.println(yeniMesaj);
+		System.out.println(mesaj.substring(2,5));
+		
+		for(String kelime : mesaj.split(" ")){
+			
+			System.out.println(kelime);
+			
+		}
+		
+		System.out.println(mesaj.toLowerCase());
+		System.out.println(mesaj.toUpperCase());
+		System.out.println(mesaj.trim());
+		
+
+	}
+
+}
