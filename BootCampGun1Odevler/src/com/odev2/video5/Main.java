6218d2ddf9ec47fe7e86e6d9122808524477816f
Serkan Güneş <gunesserkan883@gmail.com>
2022-09-15 17:57:38 +0300

    Ödev2 video5 ve video6 örnekleri.

---
+++ BootCampGun1Odevler/src/com/odev2/video5/Main.java
@@ -0,0 +1,11 @@
+package com.odev2.video5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba Java2");
+
+	}
+
+}
---
+++ BootCampGun1Odevler/src/com/odev2/video6/Main.java
@@ -0,0 +1,12 @@
+package com.odev2.video6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+
+}
