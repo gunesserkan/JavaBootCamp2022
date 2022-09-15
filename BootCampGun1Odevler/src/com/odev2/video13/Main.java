641e7880155d0db8ed677bd792d64534dc1762db
Serkan Güneş <gunesserkan883@gmail.com>
2022-09-15 18:34:38 +0300

    Odev2 Video13 do-while örnek program

---
+++ BootCampGun1Odevler/src/com/odev2/video13/Main.java
@@ -0,0 +1,21 @@
+package com.odev2.video13;
+
+public class Main {
+	//DONGULER ORNEK
+	public static void main(String[] args) {
+		//do-while dongusu
+		
+		int j=0;
+		
+		do {
+			System.out.println(j);
+			j+=2;
+		} while (j<10);
+
+		
+		System.out.println("Do-While döngüsü bitti");
+		
+		
+	}
+
+}
