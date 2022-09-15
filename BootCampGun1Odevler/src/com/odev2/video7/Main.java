f36360241860cdc4ed74fc4eb783a68154f4d0d2
Serkan Güneş <gunesserkan883@gmail.com>
2022-09-15 18:03:05 +0300

    Ödev2 Vİdeo7 örnekleri.

---
+++ BootCampGun1Odevler/src/com/odev2/video7/Main.java
@@ -0,0 +1,13 @@
+package com.odev2.video7;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi=12;
+		double sayiDouble=12.5;
+		String sehir="ANKARA";
+		char karakter = 'A';
+		boolean dogruMu = false;
+	}
+
+}
