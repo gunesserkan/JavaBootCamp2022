efafcaef00da1e1a8a9240c7f9d1cd364f316c13
Serkan Güneş <gunesserkan883@gmail.com>
2022-09-15 18:29:37 +0300

    Ödev2/Video12 while döngüsü örnek uygulama

---
+++ BootCampGun1Odevler/src/com/odev2/video12/Main.java
@@ -0,0 +1,18 @@
+package com.odev2.video12;
+
+public class Main {
+	//DONGULER ORNEK while dongusu
+	public static void main(String[] args) {
+		
+		int i=2;
+		
+		while(i<10) {
+			System.out.println(i);
+			i+=2;
+		}
+		
+		System.out.println("While döngüsü bitti");
+
+	}
+
+}
