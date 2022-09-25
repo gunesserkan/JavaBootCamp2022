75049f664737b0ecd63943ee4c1ab6cbb75699a0
Serkan Güneş <gunesserkan883@gmail.com>
2022-09-25 23:01:16 +0300

    Gun2-Odev2-video30 ReCap classlarla calismak

---
+++ BootCampGun2Odevler/src/com/Odev2/video30/Main.java
@@ -0,0 +1,12 @@
+package com.Odev2.video30;
+
+public class Main {
+	//ReCap classlarla calismak
+	public static void main(String[] args) {
+		
+		DortIslem dortislem=new DortIslem();
+		int sonuc=dortislem.topla(3, 4);
+		System.out.println(sonuc);
+	}
+
+}
