6242454411180eb3d27a214396f5d8f634d4a515
Serkan Güneş <gunesserkan883@gmail.com>
2022-09-25 22:33:30 +0300

    Gun2-Odev2-video27 Class nedir

---
+++ BootCampGun2Odevler/src/com/Odev2/video27/CustomerManager.java
@@ -0,0 +1,15 @@
+package com.Odev2.video27;
+
+public class CustomerManager {
+	public void add() {
+		System.out.println("Müşteri eklendi.");
+	}
+	
+	public void remove() {
+		System.out.println("Müşteri silindi.");
+	}
+	
+	public void update() {
+		System.out.println("Müşteri güncellendi");
+	}
+}
---
+++ BootCampGun2Odevler/src/com/Odev2/video27/Main.java
@@ -0,0 +1,11 @@
+package com.Odev2.video27;
+
+public class Main {
+	//Class nedir
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
